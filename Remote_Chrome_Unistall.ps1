84202e926ebef89c0e3844f81cb06a7488ceaa3d
jayendra05 <42347169+jayendra05@users.noreply.github.com>
2024-02-06 00:24:36 +0530

    Second commit

---
+++ Remote_Chrome_Unistall.ps1
@@ -0,0 +1,7 @@
+﻿$SEARCH = 'chrome$'
+$INSTALLED = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
+$INSTALLED += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, UninstallString
+$RESULT = $INSTALLED | ?{ $_.DisplayName -ne $null } | Where-Object {$_.DisplayName -match $SEARCH }
+$UNINSTALL_COMMAND=(($RESULT.UninstallString -split '\"')[1])
+$UNINSTALL_ARGS=(($RESULT.UninstallString -split '\"')[2]) + ' --force-uninstall'
+Start-Process $UNINSTALL_COMMAND -ArgumentList $UNINSTALL_ARGS -Wait
---
+++ invoke_Chrome_Unistall_script.ps1
@@ -0,0 +1,48 @@
+﻿Login-AzAccount -tenant ""
+$outp = @();
+$vms = Import-Csv -Path "C:\Users\Inputcsv.csv"
+
+foreach($vm in $vms)
+{
+
+Set-AzContext -Subscription $vm.Subscription
+
+$vmrg = Get-azvm -Name $vm.VMName
+
+$status = get-azvm -Name $vm.VMName -ResourceGroupName $vmrg.ResourceGroupName -Status
+
+
+     if($status.Statuses[1].DisplayStatus -eq "VM running" )
+     {
+       try
+       {
+          Write-Host $vm.VMName
+        Invoke-AzVMRunCommand -ResourceGroupName $vmrg.ResourceGroupName -VMName $vm.VMName -CommandId runpowershellscript -ScriptPath 'C:\Users\Remote_Chrome_install.ps1' -Verbose
+    
+          $outp += [pscustomobject]@{
+                             "VMName" = $vm.VMName;
+                             "ResourceGroup" = $vm.VMResourceGroup;
+                             "VM Status" = "triggered";               
+                 }
+          }
+        catch
+        {
+          $outp += [pscustomobject]@{
+                             "VMName" = $vm.VMName;
+                             "ResourceGroup" =  $vm.VMResourceGroup;
+                             "VM Status" = "error";               
+                 }
+        }
+      }
+      else
+      {
+        $outp += [pscustomobject]@{
+                             "VMName" = $vm.VMName;
+                             "ResourceGroup" = $vm.VMResourceGroup;
+                             "VM Status" = "VM not running";
+                             
+                            
+                 }
+      }
+}
+$outp | Export-Csv -NoTypeInformation -Path "‪C:\Users\uninstalloutput.csv"
